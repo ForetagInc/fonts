fd8356737c85e7b184b67b01354c77f1efd5cae4
Chiru B <chirub@foretag.co>
2022-04-11 13:08:28 +0100

    refactor: infinite scroll

---
+++ components/Cards.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+
+import type { IFont } from '../lib/interfaces';
+import { useStore } from '../lib/store';
+
+import fonts from '../public/data.json';
+
+export const Cards: React.FC = () => {
+	const { content, fontSize } = useStore(s => s);
+
+	const customMap = ['molle', 'syne-italic'];
+
+	const fontsPerPage = 12;
+	const [lastViewPosition, setLastViewPosition] = React.useState(0);
+	const [loadedFonts, setLoadedFonts] = React.useState<IFont[]>(fonts.slice(0, fontsPerPage));
+
+	React.useEffect(() => {
+		if (typeof window !== 'undefined') {
+			window.addEventListener('scroll', (e) => {
+				// @ts-ignore
+				const scrollTop: number = e.target.documentElement.scrollTop;
+				// @ts-ignore
+				const scrollHeight: number = e.target.documentElement.scrollHeight;
+
+				if (window.innerHeight + scrollTop + 1 >= scrollHeight) {
+					setLoadedFonts(
+						// @ts-ignore
+						current => {
+							return [
+								...current,
+								fonts.slice(lastViewPosition, lastViewPosition + fontsPerPage)
+							]
+						});
+
+					setLastViewPosition(current => current + fontsPerPage);
+				}
+			});
+		}
+	});
+
+	const loadStylesheet = (url: string) => {
+		if (typeof window !== 'undefined') {
+			let head = document.head;
+			let link = document.createElement('link');
+
+			link.type = 'text/css';
+			link.rel = 'stylesheet';
+			link.href = url;
+
+			head.appendChild(link);
+		}
+	}
+
+	return (
+		<div className='p:grid grid-cols:3@lg grid-cols:2@md grid-cols:1 gap:20'>
+			{
+				loadedFonts.map(
+					(font, index) => {
+						let url = customMap.includes(font.id) ? 'latin' : font.weights[0];
+
+						loadStylesheet(`https://cdn.jsdelivr.net/npm/@fontsource/${font.id}@latest/${url}.css`);
+
+						return <div
+							key={index}
+							className='b:1 b:gray-5 r:10 px:16 py:24 bg:gray-5:hover cursor:pointer f:black min-h:300'
+						>
+							<div className='d:flex justify-content:space-between w:100%'>
+								<div>
+									<p className='f:18'>{font.family}</p>
+									<small>{font.category}</small>
+								</div>
+								<small>
+									{font.weights.length} styles
+								</small>
+							</div>
+							<p className='mt:20' style={{ fontSize, fontFamily: font.family }}>
+								{content}
+							</p>
+						</div>
+					}
+				)
+			}
+		</div>
+	)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,12 @@
 import * as React from 'react';
 
 import type { NextPage } from 'next';
-import type { IFont } from './interfaces';
 
-import * as fonts from '../public/data.json';
 import { useStore } from '../lib/store';
-
-import InfiniteScroll from 'react-infinite-scroller';
+import { Cards } from '../components/Cards';
 
 const Home: NextPage = () => {
-	const customMap = ['molle', 'syne-italic'];
-
-	const fontsPerPage = 12;
-	const [lastViewPosition, setLastViewPosition] = React.useState(0);
-	const [loadedFonts, setLoadedFonts] = React.useState<IFont[]>();
-
-	const { content, updateContent, fontSize } = useStore(s => s);
-
-	const loadStylesheet = (url: string) => {
-		if (typeof window !== 'undefined') {
-			let head = document.head;
-			let link = document.createElement('link');
-
-			link.type = 'text/css';
-			link.rel = 'stylesheet';
-			link.href = url;
-
-			head.appendChild(link);
-		}
-	}
-
-	const loadFonts = (page: number) => {
-		// @ts-ignore
-		setLoadedFonts(current => {
-			return [
-				...current,
-				fonts.slice(lastViewPosition, lastViewPosition + fontsPerPage)
-			]
-		});
-
-		setLastViewPosition(current => current + fontsPerPage);
-	}
+	const { content, updateContent } = useStore(s => s);
 
 	return (
 		<div className='h:100vh f:gray-60 bg:gray-80@dark f:white@dark'>
@@ -65,43 +31,7 @@ const Home: NextPage = () => {
 						}}
 					/>
 				</div>
-
-				<div className='p:grid grid-cols:3@lg grid-cols:2@md grid-cols:1 gap:20'>
-					<InfiniteScroll
-						pageStart={0}
-						useWindow={false}
-						// loadMore={loadFonts(0)}
-						hasMore={lastViewPosition < fonts.length}
-					>
-						{
-							(fonts as unknown as IFont[]).map(
-								(font, index) => {
-									let url = customMap.includes(font.id) ? 'latin' : font.weights[0];
-
-									loadStylesheet(`https://cdn.jsdelivr.net/npm/@fontsource/${font.id}@latest/${url}.css`);
-
-									return <div
-										key={index}
-										className='b:1 b:gray-5 r:10 px:16 py:24 bg:gray-5:hover cursor:pointer f:black min-h:300'
-									>
-										<div className='d:flex justify-content:space-between w:100%'>
-											<div>
-												<p className='f:18'>{font.family}</p>
-												<small>{font.category}</small>
-											</div>
-											<small>
-												{font.weights.length} styles
-											</small>
-										</div>
-										<p className='mt:20' style={{ fontSize, fontFamily: font.family }}>
-											{content}
-										</p>
-									</div>
-								}
-							)
-						}
-					</InfiniteScroll>
-				</div>
+				<Cards />
 			</div>
 		</div>
 	)
