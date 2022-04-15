a218aca88411967714be0db29d659d3e666b28c9
Chiru B <chirub@foretag.co>
2022-04-15 11:47:34 +0100

    add: Search functionality and styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,27 +23,33 @@ const Home: NextPage = () => {
 			</div>
 
 			<div className='mx:auto w:80% py:24'>
-				<div className='mb:20 b:1 r:20 b:gray-10 p:8'>
-					<input
-						className='outline:none'
-						type='text'
-						placeholder='Search'
-						value={search}
-						onChange={e => {
-							e.preventDefault();
-							setSearch(e.target.value);
-						}}
-					/>
-					<input
-						className='outline:none min-w:300'
-						type='text'
-						placeholder='Preview'
-						value={content}
-						onChange={e => {
-							e.preventDefault();
-							updateContent(e.target.value);
-						}}
-					/>
+				<div className='d:flex mb:20 b:1 r:30 b:gray-10 px:8'>
+					<div className='d:flex align-items:center br:1 b:gray-10 py:12 min-w:300'>
+						<i className='ri-search-line mr:8 f:gray-20' />
+						<input
+							className='outline:none f:15'
+							type='text'
+							placeholder='Search fonts'
+							value={search}
+							onChange={e => {
+								e.preventDefault();
+								setSearch(e.target.value);
+							}}
+						/>
+					</div>
+					<div className='d:flex align-items:center ml:8'>
+						<small className='mr:16 bg:blue-40 f:white px:8 py:2 r:40'>Preview</small>
+						<input
+							className='outline:none min-w:300'
+							type='text'
+							placeholder='Preview'
+							value={content}
+							onChange={e => {
+								e.preventDefault();
+								updateContent(e.target.value);
+							}}
+						/>
+					</div>
 				</div>
 				<Cards
 					search={search}
