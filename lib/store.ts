a53bc906ab7b0d31f16c9d1c5adfa625a6ec26d7
Chiru B <chirub@foretag.co>
2022-04-15 13:55:06 +0100

    add: IStore interface for store

--- lib/store.ts
+++ lib/store.ts
@@ -1,8 +1,16 @@
 import Zustand from 'zustand';
 import { persist } from 'zustand/middleware';
 
-export const useStore = Zustand(persist(
+interface IStore {
+	mode: 'light' | 'dark';
+	content: string;
+	fontSize: number;
+	updateContent: (newContent: string) => void;
+};
+
+export const useStore = Zustand<IStore>(persist(
 	(set, get) => ({
+		mode: 'light',
 		content: 'Almost before we knew it, we had left the ground.',
 		fontSize: 40,
 		updateContent: (newContent: string) => set({ content: newContent })
