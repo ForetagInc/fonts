2f6969386af59e71d7d6c6e3a23fc22ce068f2f0
Chiru B <chirub@foretag.co>
2022-04-17 17:19:24 +0100

    add: Favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document: React.FC = () => {
 	return (
 		<Html>
-			<Head />
+			<Head>
+				<link rel='icon' href='/images/logo.svg' type='image/svg+xml' />
+			</Head>
 			<body className='f:gray-60 bg:fade-88@dark f:white@dark'>
 				<Main />
 				<NextScript />
