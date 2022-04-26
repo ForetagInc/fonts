ce70bc93d062da989deed1c61a197f19cff9cdb7
Chiru B <chirub@foretag.co>
2022-04-26 11:38:23 +0100

    implement: reset state

--- lib/store.ts
+++ lib/store.ts
@@ -13,25 +13,31 @@ interface IStore {
 		weight: number;
 	}>;
 
+	toInitialState: () => void;
 	toggleTheme: () => void;
 	toggleSelectBar: () => void;
 	updateContent: (newString: string) => void;
 	addSelectedFont: (family: string, weight: number) => void;
 };
 
+let initialState = {
+	isDark: typeof window !== 'undefined' ?
+		window.matchMedia('(prefers-color-scheme: dark)').matches : false,
+
+	isSelectBarOpen: false,
+
+	content: 'Almost before we knew it, we had left the ground.',
+	fontSize: 40,
+
+	selectedFonts: [],
+};
+
 export const useStore = Zustand<IStore>(persist(
 	(set, get) => ({
-		isDark: typeof window !== 'undefined' ?
-			window.matchMedia('(prefers-color-scheme: dark)').matches : false,
-
-		isSelectBarOpen: false,
-
-		content: 'Almost before we knew it, we had left the ground.',
-		fontSize: 40,
-
-		selectedFonts: [],
+		...initialState,
 
 		/* Actions */
+		toInitialState: () => set({ ...initialState }),
 		toggleTheme: () => set({ isDark: !get().isDark }),
 		toggleSelectBar: () => set({ isSelectBarOpen: !get().isSelectBarOpen }),
 		updateContent: (newContent: string) => set({ content: newContent }),
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-
-import Head from 'next/head';
 import type { NextPage } from 'next';
 
 import { useStore } from '../lib/store';
@@ -9,10 +7,9 @@ import { Layout } from '../layouts/App';
 
 const Home: NextPage = () => {
 	const {
-		toggleTheme,
-		fontSize,
 		content,
-		updateContent
+		updateContent,
+		toInitialState
 	} = useStore(s => s);
 
 	const [search, setSearch] = React.useState('');
@@ -22,10 +19,10 @@ const Home: NextPage = () => {
 			title='Browser Fonts'
 		>
 			<div className='d:flex mb:20 b:1 r:30 b:gray-10 b:gray-60@dark px:8'>
-				<div className='d:flex align-items:center br:1 b:gray-10 b:gray-60@dark min-w:300'>
+				<div className='flex-grow:0.5 d:flex align-items:center br:1 b:gray-10 b:gray-60@dark'>
 					<i className='ri-search-line mr:8 f:gray-20' />
 					<input
-						className='outline:none f:15 w:100% py:12'
+						className='outline:none f:15 py:12'
 						type='text'
 						placeholder='Search fonts'
 						value={search}
@@ -35,10 +32,10 @@ const Home: NextPage = () => {
 						}}
 					/>
 				</div>
-				<div className='d:flex align-items:center px:8 br:1 b:gray-10 b:gray-60@dark min-w:500'>
+				<div className='flex-grow:1 d:flex align-items:center px:8 br:1 b:gray-10 b:gray-60@dark'>
 					<small className='mr:16 bg:blue-40 f:white px:8 py:2 r:40'>Preview</small>
 					<input
-						className='outline:none'
+						className='outline:none w:100%'
 						type='text'
 						placeholder='Preview'
 						value={content}
@@ -46,6 +43,15 @@ const Home: NextPage = () => {
 							e.preventDefault();
 							updateContent(e.target.value);
 						}}
+					/>
+				</div>
+				<div className='flex-grow:1 br:1 b:gray-10 b:gray-60@dark'>
+					Test
+				</div>
+				<div className='d:flex align-items:center px:8'>
+					<i
+						className='ri-restart-line f:blue-40:hover cursor:pointer'
+						onClick={() => { toInitialState(); }}
 					/>
 				</div>
 			</div>
