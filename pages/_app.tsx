a6820d489276ff4f46f5f88bd59ee9ca9f3b68a9
Chiru B <chirub@foretag.co>
2022-05-07 22:44:30 +0100

    refactor(App): remove SafeHydration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,6 @@ import '@master/styles';
 
 import * as React from 'react';
 
-// const SafeHydrate: React.FC<React.PropsWithChildren<{}>> = (props) => {
-// 	return (
-// 		<div suppressHydrationWarning>
-// 			{typeof window === 'undefined' ? null : props.children}
-// 		</div>
-// 	);
-// }
-
 function App({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />
 }
