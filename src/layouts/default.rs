5e8376dac99cedabfa282d9c0fcabfce2c139e08
itsezc <chirub@foretag.co>
2022-08-20 18:00:27 +0100

    chore: remove NextJS pages & components

--- src/layouts/default.rs
+++ src/layouts/default.rs
@@ -9,26 +9,29 @@ pub struct DefaultLayoutProps {
 #[function_component]
 pub fn Layout(props: &DefaultLayoutProps) -> Html {
 	html! {
-		<div>
-			<div class="bg:gray-90 min-h:110">
-				{"test"}
-			</div>
-			<div class="flex px:64 jc:space-between ai:center bg:white bb:4|solid|gray-90 min-h:300">
-				<h1>{"Fonts"}</h1>
+		<>
+			<div>
+				<div class="bg:gray-90 min-h:110">
+					{"test"}
+				</div>
+				<div class="flex px:64 jc:space-between ai:center bg:white bb:4|solid|gray-90 min-h:300">
+					<h1>{"Fonts"}</h1>
 
-				<ul class="flex">
-					<li class="flex ai:center jc:center min-w:300 bg:black f:white h:300">
-						{"Fonts"}
-					</li>
-					<li class="flex ai:center jc:center min-w:300  f:black h:300 br:4|solid|gray-90">
-						{"Web"}
-					</li>
-				</ul>
+					<ul class="flex">
+						<li class="flex ai:center jc:center min-w:300 bg:black f:white h:300">
+							{"Fonts"}
+						</li>
+						<li class="flex ai:center jc:center min-w:300  f:black h:300 br:4|solid|gray-90">
+							{"Web"}
+						</li>
+					</ul>
 
-				<div>
-					{"No fonts selected"}
+					<div>
+						{"No fonts selected"}
+					</div>
 				</div>
 			</div>
-		</div>
+			{props.children.to_owned()}
+		</>
 	}
 }
