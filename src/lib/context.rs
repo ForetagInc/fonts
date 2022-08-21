f80b6f00a4497b348e02f01068256c249a318939
itsezc <chirub@foretag.co>
2022-08-21 10:49:31 +0100

    fix: Bounce store

--- src/lib/context.rs
+++ src/lib/context.rs
@@ -1,1 +1,7 @@
+use bounce::*;
 
+#[derive(Default, PartialEq, Eq, Atom)]
+pub struct Store {
+	pub content: String,
+	pub font_size: i32,
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ mod layouts;
 mod lib;
 mod types;
 
+use lib::context::Store;
+
+use bounce::*;
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
 use layouts::default::Layout;
@@ -10,14 +14,33 @@ use layouts::default::Layout;
 #[function_component(App)]
 fn app() -> Html {
 	html! {
-		<Index />
+		<BounceRoot>
+			<Index />
+		</BounceRoot>
 	}
 }
 
 #[function_component]
 fn Index() -> Html {
+	let store = use_atom::<Store>();
+
+	let on_text_input = {
+		let store = store.clone();
+
+		Callback::from(move |e: InputEvent| {
+			let input: HtmlInputElement = e.target_unchecked_into();
+
+			store.set(Store {
+				content: input.value(),
+				..Default::default()
+			});
+		})
+	};
+
 	html! {
-		<Layout />
+		<Layout>
+			<input type="text" placeholder="preview" oninput={on_text_input} value={store.content.to_string()} />
+		</Layout>
 	}
 }
 
